e88388918a0129e8156af7f58a8a654c2aa5c9df
GennariR <riccardo.gennari3@studio.unibo.it>
2020-04-18 16:05:07 +0200

    WinCondition class now has a public static method to get the enum value(WinCondition) from its name field(String)

--- src/main/java/model/WinCondition.java
+++ src/main/java/model/WinCondition.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.NoSuchElementException;
+
 public enum WinCondition {
     ALL_ENEMY_SHIPS_SUNK("Classic","Sink all enemy ships to win"),
     HIGHEST_SCORE_AFTER_N_TURNS("Highest score","Have the highest score at the end of the match");
@@ -18,5 +20,14 @@ public enum WinCondition {
     
     public String getDescription() {
         return description;
+    }
+    
+    public static WinCondition getWinConditionFromName(String name) {
+        for(WinCondition wc : WinCondition.values()) {
+            if(wc.name.equals(name)) {
+                return wc;
+            }
+        }
+        throw new NoSuchElementException();
     }
 }
