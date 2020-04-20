0daa77cfde64343117c346d97a73dde13fee44f2
GennariR <riccardo.gennari3@studio.unibo.it>
2020-04-20 13:16:02 +0200

    refined method implementations and updated javadoc

--- src/main/java/model/MatchManager.java
+++ src/main/java/model/MatchManager.java
@@ -1,8 +1,5 @@
 package model;
 
-
-import java.util.Set;
-
 /**
  * This interface is used to start a new match by calling the start() method.
  * The new match will be managed from start to finish by this interface, using the settings
@@ -11,9 +8,7 @@ import java.util.Set;
 public interface MatchManager {
     /**
      * This method is used to start a new match.
-     * Once a match is started, the start() method cannot be called again until the match is over.
-     * @param players a Set containing the players
-     * @param wc the win conditions of the match (decides when the match is considered over)
+     * Once a match is started, the start() method cannot be called again from the same MatchManager instance.
      */
-    void start(Set<Player> players, WinCondition wc);
+    void start();
 }
--- src/main/java/model/MatchManagerImpl.java
+++ src/main/java/model/MatchManagerImpl.java
@@ -4,34 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-public class MatchManagerImpl implements MatchManager {
+/**
+ * Implementation of MatchManager interface.
+ */
+public final class MatchManagerImpl implements MatchManager {
 
     //TODO Consider making this class a singleton 
     // VERY UNFINISHED CLASS !!
     // will handle the gameloop
-    
+
     private boolean isMatchStarted;
-    private  List<Player> players;
-    private  WinCondition wc;
-    
-    public MatchManagerImpl() {
+    private  final List<Player> players;
+    private  final WinCondition wc;
+
+    /**
+     * This is the class' constructor.
+     * @param players - a Set containing the players of the match
+     * @param wc - the winCondition which determines when the match will end
+     */
+    public MatchManagerImpl(final Set<Player> players, final WinCondition wc) {
         this.isMatchStarted = false;
+        this.players = new ArrayList<>(players);
+        this.wc = wc;
     }
-    
+
     @Override
-    public void start(Set<Player> players, WinCondition wc) {
-        if(isMatchStarted) {
+    public void start() {
+        if (isMatchStarted) {
             throw new IllegalStateException("A match is already in progress");
         }
         isMatchStarted = true;
-        this.wc = wc; 
-        this.players = new ArrayList<>(players);
         gameLoop();
     }
-    
+
     private void gameLoop() {
-        while(true) {
-            for(int i=0; i<players.size(); i++) {
+        while (true) {
+            for (int i = 0; i < players.size(); i++) {
                 // load grid for player(i)
                 players.get(i).startTurn();
                 // if(isMatchOver(wc)) break;
