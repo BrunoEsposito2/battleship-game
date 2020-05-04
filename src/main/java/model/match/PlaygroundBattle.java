252f0b0013cac30f3ca4b71578e2a527e6cfbcdd
Angelo <angelo.tinti@studio.unibo.it>
2020-05-05 01:15:13 +0200

    src/main/java/model/match/PlaygroundBattle.java - New methods added.

--- src/main/java/model/match/PlaygroundBattle.java
+++ src/main/java/model/match/PlaygroundBattle.java
@@ -1,10 +1,17 @@
 package model.match;
 
+import model.util.Pair;
+
 /**
- * 
- * @author angel
- *
+ * Interface that handle the playground of battleship.
  */
 public interface PlaygroundBattle {
 
+    boolean positionShip(ShipAngelo ship, Pair<Integer, Integer> box);
+
+    boolean isPositionable(ShipAngelo ship, Pair<Integer, Integer> box);
+
+    boolean removeShip(Pair<Integer, Integer> box);
+
+    boolean shot(Pair<Integer, Integer> box);
 }
