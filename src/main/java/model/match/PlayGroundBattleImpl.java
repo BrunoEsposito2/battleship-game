be4be82928b38de8290442e4812b4a80e59239c7
Angelo Tinti <angelo.tinti@studio.unibo.it>
2020-05-06 02:23:12 +0200

    src/main/java/model/match/PlayGroundBattleImpl.java - Created.

---
+++ src/main/java/model/match/PlayGroundBattleImpl.java
@@ -0,0 +1,66 @@
+package model.match;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.util.Pair;
+
+/**
+ *
+ */
+public class PlayGroundBattleImpl implements PlaygroundBattle {
+
+    private List<List<Boolean>> playground;
+    private int columns;
+    private int lines;
+
+    /**
+     * Constructor of battle's playground with size passed.
+     * @param size - number of box for side.
+     */
+    public PlayGroundBattleImpl(final int columns, final int lines) {
+        this.columns = columns;
+        this.lines = lines;
+        this.createPlayGround();
+    }
+
+    @Override
+    public boolean positionShip(final ShipAngelo ship, final Pair<Integer, Integer> box) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isPositionable(final ShipAngelo ship, final Pair<Integer, Integer> box) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean removeShip(final Pair<Integer, Integer> box) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean shot(final Pair<Integer, Integer> box) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean areThereAliveShip() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+    
+    private void createPlayGround() {
+        this.playground = new ArrayList<List<Boolean>>(this.columns);
+        for (int i = 0; i < this.columns ; i++) {
+            this.playground.add(new ArrayList<Boolean>(this.lines));
+            for (int j = 0; j < this.lines; j++) {
+                this.playground.get(j).add(false);
+            }
+        }
+    }
+}
