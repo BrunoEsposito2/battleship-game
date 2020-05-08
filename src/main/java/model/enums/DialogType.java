540cbf1f6680b691a9ba49bf09be79b21b02f015
GennariR <riccardo.gennari3@studio.unibo.it>
2020-05-08 23:40:13 +0200

    set files to use correct line delimiter

--- src/main/java/model/enums/DialogType.java
+++ src/main/java/model/enums/DialogType.java
@@ -2,12 +2,30 @@ package model.enums;
 
 import javafx.scene.control.Alert.AlertType;
 
+/**
+ * This enum represents the Dialog types that DialogBuilder can use. 
+ */
 public enum DialogType {
 
+    /**
+     * Ask confirmation from user.
+     */
     CONFIRMATION(AlertType.CONFIRMATION),
+    /**
+     * Give the user information. 
+     */
     INFORMATION(AlertType.INFORMATION),
+    /**
+     * Warn the user about a non-critical issue.
+     */
     WARNING(AlertType.WARNING),
+    /**
+     * Warn the user about a critical issue.
+     */
     ERROR(AlertType.ERROR),
+    /**
+     * Ask the user for a password.
+     */
     LOGIN(AlertType.NONE);
 
     private final AlertType concreteType;
@@ -16,6 +34,9 @@ public enum DialogType {
         this.concreteType = concreteType;
     }
 
+    /**
+     * @return the concrete type of this DialogType
+     */
     public AlertType getConcreteType() {
         return concreteType;
     }
--- src/main/java/view/SceneManager.java
+++ src/main/java/view/SceneManager.java
@@ -2,7 +2,6 @@ package view;
 
 import java.io.File;
 import application.Main;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.enums.SceneName;
@@ -26,7 +25,7 @@ public enum SceneManager {
      * @param nextScene - the name of the Scene you want to load.
      */
     public void switchScene(final SceneName nextScene) {
-        currentStage.setScene(new Scene( loader.loadFXML("layouts" + File.separator + nextScene.getLayoutName() + ".fxml")));
+        currentStage.setScene(new Scene(loader.loadFXML("layouts" + File.separator + nextScene.getLayoutName() + ".fxml")));
     }
 
 }
