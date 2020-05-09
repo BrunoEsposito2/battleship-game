69d4bd4d27aaf9111e5a60a6be8eac63ff91b47c
Angelo Tinti <angelo.tinti@studio.unibo.it>
2020-05-09 23:12:58 +0200

    src/main/java/model/enums/Orientation.java - Add constructor of cells occupied.

--- src/main/java/model/enums/Orientation.java
+++ src/main/java/model/enums/Orientation.java
@@ -1,6 +1,11 @@
 package model.enums;
 
 import model.util.Pair;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
 
 /**
  * 
@@ -9,20 +14,27 @@ import model.util.Pair;
  *
  */
 public enum Orientation {
-    
+
     /**
      * Orientation is horizontal.
      */
     HORIZONTAL(new Pair<>(0, 1)),
-    
+
     /**
      * Orientation in vertical.
      */
-    VERTICAL(new Pair<>(1,0));
+    VERTICAL(new Pair<>(1, 0));
 
     private Pair<Integer, Integer> direction;
 
     Orientation(final Pair<Integer, Integer> direction) {
         this.direction = direction;
+    }
+
+    public List<Pair<Integer, Integer>> occupied(final Pair<Integer, Integer> initialBox, final int size) {
+        return Stream.iterate(initialBox, 
+                        i -> new Pair<Integer, Integer>(i.getX() + this.direction.getX(), i.getY() + this.direction.getY()))
+                .limit(size)
+                .collect(toList());
     }
 }
