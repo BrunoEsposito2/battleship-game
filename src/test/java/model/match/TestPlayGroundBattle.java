9df83cb6adc22a44274cae44b13e439b96b45d13
Angelo Tinti <angelo.tinti@studio.unibo.it>
2020-05-12 01:58:48 +0200

    src/test/java/model/match/TestPlayGroundBattle.java - Test for insertion's ship added.

--- src/test/java/model/match/TestPlayGroundBattle.java
+++ src/test/java/model/match/TestPlayGroundBattle.java
@@ -1,22 +1,36 @@
 package model.match;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import model.enums.Orientation;
+import model.enums.ShipType;
+import model.util.Pair;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
 public class TestPlayGroundBattle {
 
+    private static int COLUMNS = 10;
+    private static int LINES = 8;
+    private static int FIRST = 0;
+    private static int CELLS_USED = 3;
+
+
+    private static PlaygroundBattle PLAYGROUND_BATTLE = null;
+
+    @BeforeAll
+    public static void initPlaygroundBattle() {
+        PLAYGROUND_BATTLE = new PlaygroundBattleImpl(LINES, COLUMNS);
+    }
+
     @Test
     public void testCreationPlayground() {
-        int columns = 10;
-        int lines = 8;
-        int first = 0;
 
-        PlaygroundBattle playgroundBattle = new PlaygroundBattleImpl(lines, columns);
-
-        List<List<Boolean>> playground = playgroundBattle.getPlaygroundBattle();
+        List<List<Boolean>> playground = PLAYGROUND_BATTLE.getPlaygroundBattle();
 
         playground.stream()
                   .forEach(
@@ -24,8 +38,16 @@ public class TestPlayGroundBattle {
                                 .forEach(
                                         y -> assertEquals(y, false)));
         System.out.println(playground);
-        assertEquals(lines, playground.size());
-        assertEquals(columns, playground.get(first).size());
+        assertEquals(LINES, playground.size());
+        assertEquals(COLUMNS, playground.get(FIRST).size());
     }
+
+    @Test
+    public void testInsertShip() {
+        assertTrue(PLAYGROUND_BATTLE.positionShip(new Ship(3, ShipType.BATTLESHIP),
+                new Pair<Integer, Integer>(0, 0), Orientation.HORIZONTAL));
+    }
+
+
 
 }
