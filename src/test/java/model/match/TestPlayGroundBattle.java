0a203c3c6c12dabc4f9a680e97732ededc24fdc3
Angelo Tinti <angelo.tinti@studio.unibo.it>
2020-05-13 01:23:27 +0200

    src/test/java/model/match/TestPlayGroundBattle.java - testInsertMultipleShip added.

--- src/test/java/model/match/TestPlayGroundBattle.java
+++ src/test/java/model/match/TestPlayGroundBattle.java
@@ -1,6 +1,7 @@
 package model.match;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import model.enums.Orientation;
@@ -20,11 +21,16 @@ public class TestPlayGroundBattle {
     private static int FIRST = 0;
     private static int CELLS_USED = 3;
 
+    private static Ship FIRST_SHIP = new Ship(1, ShipType.BATTLESHIP);
+    private static Ship SECOND_SHIP = new Ship(2, ShipType.CRUISER);
+    private static Ship THIRD_SHIP = new Ship(3, ShipType.DESTROYER);
+    private static Ship FOURTH_SHIP = new Ship(4, ShipType.SUBMARINE);
+
 
     private static PlaygroundBattle PLAYGROUND_BATTLE = null;
 
-    @BeforeAll
-    public static void initPlaygroundBattle() {
+    @BeforeEach
+    public void initPlaygroundBattle() {
         PLAYGROUND_BATTLE = new PlaygroundBattleImpl(LINES, COLUMNS);
     }
 
@@ -55,7 +61,7 @@ public class TestPlayGroundBattle {
          * To verify that list of overlapped cells is not empty.
          */
         assertFalse((PLAYGROUND_BATTLE.getCellsOverlappedList(cells).isEmpty()));
-        
+
         /*
          * To verify that list of overlapped cells is equal to cells already used.
          */
@@ -67,6 +73,24 @@ public class TestPlayGroundBattle {
                 new Pair<Integer, Integer>(0, 0), Orientation.HORIZONTAL));
     }
 
+    @Test
+    public void testInsertMultipleShip() {
+        Pair<Integer, Integer> firstShipPosition = new Pair<Integer, Integer>(1, 1);
 
+        Pair<Integer, Integer> secondShipPositionNotCorrect = new Pair<Integer, Integer>(2, 0);
+        Orientation secondShipOrientationNotCorrect = Orientation.HORIZONTAL;
+
+        Pair<Integer, Integer> secondShipPositionCorrect = new Pair<Integer, Integer>(0, 0);
+
+        Pair<Integer, Integer> thirdShipPosition = new Pair<Integer, Integer>(0, 0);
+        Pair<Integer, Integer> fourthShipPosition = new Pair<Integer, Integer>(0, 0);
+
+        assertTrue(PLAYGROUND_BATTLE.positionShip(THIRD_SHIP,
+                new Pair<Integer, Integer>(1, 1), Orientation.VERTICAL));
+
+        assertFalse(PLAYGROUND_BATTLE.positionShip(THIRD_SHIP,
+                secondShipPositionNotCorrect, secondShipOrientationNotCorrect));
+
+    }
 
 }
