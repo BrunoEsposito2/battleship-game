f0e2985a9f670288ef75b5230e401faa64012f92
Marco Biagini <marco.biagini8@studio.unibo.it>
2020-05-13 13:28:52 +0200

    created ShipDeployment controller

---
+++ src/main/java/controller/ui/ShipDeployment.java
@@ -0,0 +1,19 @@
+package controller.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class ShipDeployment {
+    
+    @FXML
+    private ImageView carrier, battleship, cruiser,
+                      submarine, destroyer;
+    
+    /**
+     * This method is called automatically when loading the fxml layout
+     */
+    public void initialize() {
+        
+    }
+
+}
