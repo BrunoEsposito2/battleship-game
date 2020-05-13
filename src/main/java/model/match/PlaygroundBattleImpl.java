fe7654615e76a90ee32ca2a4f2a555304312193c
Angelo Tinti <angelo.tinti@studio.unibo.it>
2020-05-14 01:36:19 +0200

    src/main/java/model/match/PlaygroundBattleImpl.java - removeShip() implemented, getCellOverlappedList() parameters modified.

--- src/main/java/model/match/PlaygroundBattleImpl.java
+++ src/main/java/model/match/PlaygroundBattleImpl.java
@@ -2,9 +2,11 @@ package model.match;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import model.enums.Orientation;
 import model.util.Pair;
@@ -39,7 +41,7 @@ public class PlaygroundBattleImpl implements PlaygroundBattle {
         /*
          * If list is empty there aren't overlap.
          */
-        if (this.getCellsOverlappedList(cellsNecessary).isEmpty()) {
+        if (this.getCellsOverlappedList(ship, firstCell, orientation).isEmpty()) {
             this.shipList.put(ship, cellsNecessary);
             cellsNecessary.forEach(i -> this.playground.get(i.getX()).set(i.getY(), true));
             return true;
@@ -48,17 +50,28 @@ public class PlaygroundBattleImpl implements PlaygroundBattle {
     }
 
     @Override
-    public List<Pair<Integer, Integer>> getCellsOverlappedList(final List<Pair<Integer, Integer>> cellsNecessary) {
-        List<Pair<Integer, Integer>> cellsAlreadyUsed = cellsNecessary.stream()
+    public List<Pair<Integer, Integer>> getCellsOverlappedList(final Ship ship, final Pair<Integer, Integer> firstCell, final Orientation orientation) {
+        List<Pair<Integer, Integer>> cellsAlreadyUsed = orientation.cellsUsedList(firstCell, ship.getSize()).stream()
                                               .filter(i -> isCellUsed(i))
                                               .collect(toList());
         return cellsAlreadyUsed;
     }
 
     @Override
-    public boolean removeShip(final Pair<Integer, Integer> box) {
-        // TODO Auto-generated method stub
-        return false;
+    public boolean removeShip(final Pair<Integer, Integer> cell) {
+        Set<Entry<Ship, List<Pair<Integer, Integer>>>> setOfShipEntries = this.shipList.entrySet();
+        Iterator<Entry<Ship, List<Pair<Integer, Integer>>>> iterator = setOfShipEntries.iterator();
+
+        while (iterator.hasNext()) {
+            Entry<Ship, List<Pair<Integer, Integer>>> ship = iterator.next();
+            List<Pair<Integer, Integer>> shipCells = ship.getValue();
+
+            if (shipCells.contains(cell)) {
+                shipCells.forEach(i -> this.playground.get(i.getX()).set(i.getY(), false));
+                iterator.remove();
+            }
+        }
+        return true;
     }
 
     @Override
@@ -91,11 +104,11 @@ public class PlaygroundBattleImpl implements PlaygroundBattle {
     public ArrayList<List<Boolean>> getPlaygroundBattle() {
         return new ArrayList<List<Boolean>>(this.playground);
     }
-
+    
     private boolean isCellUsed(final Pair<Integer, Integer> cell) {
         return this.playground.get(cell.getX()).get(cell.getY());
     }
-    
+
     private void createPlayGround() {
         this.playground = new ArrayList<List<Boolean>>(this.lines);
         for (int i = 0; i < this.lines; i++) {
