aba07b200dafd58ae7b47268a61bd299a091eb48
Angelo Tinti <angelo.tinti@studio.unibo.it>
2020-05-22 17:12:33 +0200

    src/main/java/controller/game/MatchControllerImpl.java - Created.

---
+++ src/main/java/controller/game/MatchControllerImpl.java
@@ -0,0 +1,19 @@
+package controller.game;
+
+import model.match.PlaygroundBattle;
+import model.util.Pair;
+
+public class MatchControllerImpl implements MatchController {
+
+    private PlaygroundBattle playground;
+
+    public MatchControllerImpl() {
+
+    }
+
+    @Override
+    public void shot(final int line, final int col) {
+
+    }
+
+}
