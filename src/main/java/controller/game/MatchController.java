ff32cad675dd56319186435b4623967604361ae4
Angelo Tinti <angelo.tinti@studio.unibo.it>
2020-05-22 17:18:01 +0200

    src/main/java/controller/game/MatchController.java - Implementation started.

--- src/main/java/controller/game/MatchController.java
+++ src/main/java/controller/game/MatchController.java
@@ -1,5 +1,13 @@
 package controller.game;
 
-public interface GameController {
+public interface MatchController {
+
+    /**
+     * Method for shot to enemy playground.
+     * 
+     * @param line - vertical position of shot.
+     * @param col - horizontal position of shot.
+     */
+    void shot(int line, int col);
 
 }
