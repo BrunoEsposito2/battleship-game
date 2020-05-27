6484f7f72fbc5f3901b698cbb906e819af325b3c
Angelo Tinti <angelo.tinti@studio.unibo.it>
2020-05-28 01:07:43 +0200

    src/main/java/view/match/BattleView.java - Added method for winner dialog.

--- src/main/java/view/match/BattleView.java
+++ src/main/java/view/match/BattleView.java
@@ -1,8 +1,11 @@
 package view.match;
 
+import model.enums.Player;
 import model.util.Pair;
 
 public interface BattleView {
 
     void showCellAlreadyShottedAlert(Pair<Integer, Integer> cell);
+
+    void showWinDialog(Player winnerPlayer);
 }
