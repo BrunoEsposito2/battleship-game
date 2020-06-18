21d8d20736439135ab2f1243ac4bf3ce00158368
GennariR <riccardo.gennari3@studio.unibo.it>
2020-06-19 00:00:30 +0200

    added implementation to application Controller

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,7 +1,30 @@
 package controller;
 
+import java.util.Optional;
+
+import view.dialog.DialogType;
+import view.scene.SceneName;
+
+/**
+ * The MVC Controller of the app.
+ */
 public interface Controller {
 
-    void setAccountObserver();
+    /**
+     * Load a different scene into the application's stage.
+     * @param name - the scene's name
+     */
+    void changeScene(SceneName name);
+
+    /**
+     * Launch a dialog using the passed parameters.
+     * Parameters you don't care about can be passed as null.
+     * @param type - the type of dialog to launch.
+     * @param title - dialog's title
+     * @param header - dialog's header
+     * @param description - dialog's description
+     * @return the result of the dialog's operations, if any.
+     */
+    Optional<String> launchDialog(DialogType type, String title, String header, String description);
 
 }
--- src/main/java/controller/ControllerImpl.java
+++ src/main/java/controller/ControllerImpl.java
@@ -1,23 +1,46 @@
 package controller;
 
-import controller.ui.ProfileController;
-import model.Model;
-import view.View;
+import java.util.Optional;
 
-public class ControllerImpl implements Controller {
+import controller.ui.ProfileController;
+import javafx.stage.Stage;
+import model.Model;
+import model.ModelImpl;
+import view.View;
+import view.ViewImpl;
+import view.dialog.DialogType;
+import view.scene.SceneName;
+
+/**
+ * Concrete implementation of the app's mvc controller.
+ */
+public final class ControllerImpl implements Controller {
 
     private final Model model;
     private final View view;
 
     private ProfileController profileCtrl;
 
-    public ControllerImpl(final Model model, final View view) {
-        this.model = model;
-        this.view = view;
+    public ControllerImpl(final Stage stage) {
+        view = new ViewImpl(stage);
+        model = new ModelImpl();
+        setAccountObserver();
+        //this.accountMng = new AccountOperation();
+        //ManagerInstance.getInstance();
     }
 
-    public final void setAccountObserver() {
+    private void setAccountObserver() {
         this.profileCtrl = new ProfileController();
+    }
+
+    @Override
+    public void changeScene(final SceneName name) {
+        view.loadScene(name);
+    }
+
+    @Override
+    public Optional<String> launchDialog(final DialogType type, final String title, final String header, final String description) {
+        return view.launchDialog(type, title, header, description);
     }
 
 }
