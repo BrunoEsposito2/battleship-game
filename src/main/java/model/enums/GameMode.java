a67609e8826caffba036bb86b007b5889e0b0dde
GennariR <riccardo.gennari3@studio.unibo.it>
2020-06-21 22:51:28 +0200

    completed implementation + documentation of GameMode enum

--- src/main/java/model/enums/GameMode.java
+++ src/main/java/model/enums/GameMode.java
@@ -5,18 +5,23 @@ package model.enums;
  */
 public enum GameMode {
 
+    /**
+     * Sink all enemy ships to win.
+     */
     CLASSIC("Classic", "Sink all enemy ships to win.") {
         @Override
-        public boolean checkWinCondition() {
-            //TODO add implementation
-            return true;
+        public boolean isWinConditionAchieved(final int playerHits, final int opponentHits, final int opponentRemainingShips) {
+            return opponentRemainingShips < 1;
         }
     },
+
+    /**
+     * Have the highest score at the end of the match.
+     */
     HIGH_SCORE("High-score", "Have the highest score at the end of the match.") {
         @Override
-        public boolean checkWinCondition() {
-            //TODO add implementation
-            return true;
+        public boolean isWinConditionAchieved(final int playerHits, final int opponentHits, final int opponentRemainingShips) {
+            return playerHits > opponentHits;
         }
     };
 
@@ -28,20 +33,24 @@ public enum GameMode {
         this.description = description;
     }
 
-    public boolean checkWinCondition() {
-        // this always gets overridden
-        return true;
-    }
+    /**
+     * this method checks whether the player has won the match according to selected win conditions.
+     * @param playerHits - how many times the player has hit the opponent's ships.
+     * @param opponentHits - how many times the opponent has hit the player's ships.
+     * @param opponentRemainingShips - how many (not sunk) ships the opponent still has.
+     * @return true - if the player has won the match.
+     */
+    public abstract boolean isWinConditionAchieved(int playerHits, int opponentHits, int opponentRemainingShips);
 
     /**
-     * @return the GameMode's name
+     * @return the GameMode's name.
      */
     public String getName() {
         return name;
     }
 
     /**
-     * @return the GameMode's description
+     * @return the GameMode's description.
      */
     public String getDescription() {
         return description;
