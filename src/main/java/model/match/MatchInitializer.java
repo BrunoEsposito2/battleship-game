582056ab4915249170151089a122b23d45eab5dc
GennariR <riccardo.gennari3@studio.unibo.it>
2020-06-22 19:53:14 +0200

    implemented start game method in MatchInitializer

--- src/main/java/model/match/MatchInitializer.java
+++ src/main/java/model/match/MatchInitializer.java
@@ -5,7 +5,7 @@ import application.Battleships;
 import controller.Controller;
 import model.enums.GameMode;
 import model.enums.PlayerType;
-import view.dialog.DialogType;
+import view.scene.SceneName;
 
 /**
  * Implementation of MatchManager interface.
@@ -13,7 +13,6 @@ import view.dialog.DialogType;
 public final class MatchInitializer {
 
     private final Controller controller = Battleships.getController();
-    private final GameMode gameMode;
     private final boolean aiPlayer;
 
     /**
@@ -24,26 +23,15 @@ public final class MatchInitializer {
      * @param gameMode - selected game mode.
      */
     public MatchInitializer(final String username1, final Optional<String> username2, final PlayerType playerType, final GameMode gameMode) {
-        this.aiPlayer = playerType.equals(PlayerType.HUMAN) ? true : false;
-        this.gameMode = gameMode;
-        //TODO remove debug function.
-        this.debugConstructorData(username1, username2);
-    }
-
-    private void debugConstructorData(final String username1, final Optional<String> username2) {
-        controller.launchDialog(DialogType.INFORMATION, "[DEBUG] Match Started", "[DEBUG] Match is successfully started with these settings:",
-                "player1: " + username1 + "\n"
-              + "player2: " + username2.orElse("Ai") + "\n"
-              + "gamemode: " + gameMode.getName());
+        aiPlayer = playerType.equals(PlayerType.HUMAN) ? true : false;
+        controller.setGameMode(gameMode);
     }
 
     /**
      * Start a new match.
      */
     public void startNewMatch() {
-      //TODO call ship deployment for player1
-      //TODO call ship deployment for player2
-      //TODO start match with selection data
+      controller.changeScene(SceneName.SHIP_DEPLOYMENT);
     }
 
 }
