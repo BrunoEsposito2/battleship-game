018a6222213b83a3a8f03fb4800393ba42caa027
Angelo Tinti <angelo.tinti@studio.unibo.it>
2020-06-24 18:15:42 +0200

    src/main/java/view/match/BattleViewImpl.java - Modified draw method to use graphic thread.

--- src/main/java/view/match/BattleViewImpl.java
+++ src/main/java/view/match/BattleViewImpl.java
@@ -1,10 +1,12 @@
 package view.match;
 
 import java.util.List;
+import java.util.Optional;
 
 import application.Battleships;
 import controller.game.MatchController;
 import controller.game.MatchControllerImpl;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -23,37 +25,36 @@ public class BattleViewImpl implements BattleView {
 
     @FXML
     private GridPane playerOneGrid, playerTwoGrid;
-    
+
     private GridPane currentPlayerGridPane;
     private GridPane currentVillainGridPane;
-    
-    private PlayerNumber playerNumber;
-    
-    
-    private MatchController controller;
-    
 
-    
+    private PlayerNumber playerNumber;
+
+    private MatchController controller;
+
     private void initGridPane(final GridPane gridPane) {
         for (int col = 0; col < gridPane.getColumnCount(); col++) {
             for (int row = 0; row < gridPane.getRowCount(); row++) {
                 Pane pane = new Pane();
                 final int finalRow = row, finalCol = col;
                 pane.setStyle("-fx-background-color: #FFFFFF");
-//                pane.setOnMouseEntered(e -> {
-//                    System.out.printf("Mouse enetered cell");
-//                    pane.setStyle("-fx-background-color: #ff3333");
-//                });
+                // pane.setOnMouseEntered(e -> {
+                // System.out.printf("Mouse enetered cell");
+                // pane.setStyle("-fx-background-color: #ff3333");
+                // });
                 pane.setOnMouseClicked(e -> {
-//                    Image img = new Image(getClass().getResourceAsStream("Cruiser.png"));
-//                    ImageView imageView = new ImageView(img);
-//                    imageView.fitWidthProperty().bind(pane.widthProperty());
-//                    imageView.fitHeightProperty().bind(pane.heightProperty());
-//                    playerOneGrid.add(imageView, jey, ii, 1, 5);
-//                    pane.setStyle("-fx-background-image: url(\"prova1.png\")");
-//                    pane.setStyle("-fx-background-color: #f0f0f0");
+                    // Image img = new Image(getClass().getResourceAsStream("Cruiser.png"));
+                    // ImageView imageView = new ImageView(img);
+                    // imageView.fitWidthProperty().bind(pane.widthProperty());
+                    // imageView.fitHeightProperty().bind(pane.heightProperty());
+                    // playerOneGrid.add(imageView, jey, ii, 1, 5);
+                    // pane.setStyle("-fx-background-image: url(\"prova1.png\")");
+                    // pane.setStyle("-fx-background-color: #f0f0f0");
                     if (pane.getParent().equals(this.currentVillainGridPane)) {
-                        this.controller.shot(finalRow, finalCol);
+                        new Thread(() -> {
+                            this.controller.shot(finalRow, finalCol);
+                        }).start();
                     }
                 });
                 gridPane.add(pane, col, row);
@@ -73,59 +74,76 @@ public class BattleViewImpl implements BattleView {
         this.controller.setView(this);
     }
 
-    
     /**
      * {@inheritDoc}
      */
     @Override
     public void showCellAlreadyShottedAlert(final Pair<Integer, Integer> cell) {
         final String description = "Cell [line, column]: [" + cell.getX() + "," + cell.getY() + "] is already shotted.\n"
-                                    + "Select another cell, please."; 
-        Battleships.getController().launchDialog(DialogType.WARNING, "Choiche not valid", "Cell choiced is already shotted!", description);
+                + "Select another cell, please.";
+        Battleships.getController().launchDialog(DialogType.WARNING, "Choiche not valid", "Cell choiced is already shotted!",
+                description);
     }
 
     @Override
     public void showCellAlreadyUsedAlert(final List<Pair<Integer, Integer>> cell) {
-        String description = "Cell [line, column]: ";
-        description += cell.stream().map(e -> "[" + e.getX() + "," + e.getY() + "]").collect(joining(","));
-        description += " already used." + "\n" + "Select a different place, please.";
-        Battleships.getController().launchDialog(DialogType.WARNING, "Choiche not valid", "Position choiced is already used!", description);
+        final String description = "Cell [line, column]: "
+                + cell.stream().map(e -> "[" + e.getX() + "," + e.getY() + "]").collect(joining(",")) + " already used." + "\n"
+                + "Select a different place, please.";
+        Battleships.getController().launchDialog(DialogType.WARNING, "Choiche not valid", "Position choiced is already used!",
+                description);
     }
 
     @Override
     public void showWinDialog(final PlayerNumber winnerPlayer) {
-       
+        final String description = "Il giocatore " + (winnerPlayer == PlayerNumber.PLAYER_ONE ? "uno" : "due") + " vince la partita!";
+        final Optional<String> returnDialog = Battleships.getController().launchDialog(DialogType.INFORMATION, "Fine partita",
+                "Vittoria!", description);
+        System.out.println(returnDialog.orElse("Non Presente"));
     }
 
     @Override
     public void drawHit(final Pair<Integer, Integer> pair) {
-        this.getNodeByRowColumnIndex(pair.getX(), pair.getY(), this.currentVillainGridPane).setStyle("-fx-background-color: #ff6600");
+        Platform.runLater(() -> {
+            this.getNodeByRowColumnIndex(pair.getX(), pair.getY(), this.currentVillainGridPane)
+                    .setStyle("-fx-background-color: #ff6600");
+        });
     }
-    
-//    @Override
-//    public void drawShip(nave da disegnare e posizioni) {
-//        
-//    }
-    
+
+    // @Override
+    // public void drawShip(nave da disegnare e posizioni) {
+    //
+    // }
+
     @Override
     public void drawShip(final List<Pair<Integer, Integer>> cells) {
-        for (Pair<Integer, Integer> cell : cells) {
-            this.getNodeByRowColumnIndex(cell.getX(), cell.getY(), this.currentVillainGridPane).setStyle("-fx-background-color: #00995c");
+        for (final Pair<Integer, Integer> cell : cells) {
+            Platform.runLater(() -> {
+                this.getNodeByRowColumnIndex(cell.getX(), cell.getY(), this.currentVillainGridPane)
+                        .setStyle("-fx-background-color: #00995c");
+            });
         }
     }
 
     @Override
     public void drawSunkShip(final ShipType shipType, final List<Pair<Integer, Integer>> cells) {
-        for (Pair<Integer, Integer> cell : cells) {
-            this.getNodeByRowColumnIndex(cell.getX(), cell.getY(), this.currentVillainGridPane).setStyle("-fx-background-color: #660033");
+        for (final Pair<Integer, Integer> cell : cells) {
+            Platform.runLater(() -> {
+                this.getNodeByRowColumnIndex(cell.getX(), cell.getY(), this.currentVillainGridPane)
+                        .setStyle("-fx-background-color: #660033");
+            });
         }
     }
 
     @Override
     public void drawMissed(final Pair<Integer, Integer> pair) {
-        this.getNodeByRowColumnIndex(pair.getX(), pair.getY(), this.currentVillainGridPane).setStyle("-fx-background-color: #329ef9");
+        Platform.runLater(() -> {
+            this.getNodeByRowColumnIndex(pair.getX(), pair.getY(), this.currentVillainGridPane)
+                    .setStyle("-fx-background-color: #329ef9");
+        });
     }
-    
+
+    @Override
     public void changePlayer() {
         if (this.playerNumber == PlayerNumber.PLAYER_ONE) {
             this.currentPlayerGridPane = playerOneGrid;
@@ -135,12 +153,12 @@ public class BattleViewImpl implements BattleView {
             this.currentVillainGridPane = playerOneGrid;
         }
     }
-    
-    public Node getNodeByRowColumnIndex (final int row, final int column, GridPane gridPane) {
-        Node result = null;
-        ObservableList<Node> childrens = gridPane.getChildren();
 
-        for (Node node : childrens) {
+    private Node getNodeByRowColumnIndex(final int row, final int column, GridPane gridPane) {
+        Node result = null;
+        final ObservableList<Node> childrens = gridPane.getChildren();
+
+        for (final Node node : childrens) {
             if (GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == column) {
                 result = node;
                 return result;
