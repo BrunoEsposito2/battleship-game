07bc910ba24b660829d4662a7cbb9fa813b21458
GennariR <riccardo.gennari3@studio.unibo.it>
2020-06-25 13:34:29 +0200

    added interface to PlayerInfo

--- src/main/java/model/match/players/PlayerInfo.java
+++ src/main/java/model/match/players/PlayerInfo.java
@@ -6,42 +6,19 @@ import model.enums.PlayerType;
 /**
  * This class contains info on a player during a match.
  */
-public final class PlayerInfo {
-
-    private final String username;
-    private final PlayerType type;
-    private final PlayerNumber number;
-
+public interface PlayerInfo {
     /**
-     * Constructor of this class.
-     * @param username - player's username
-     * @param type - player type
-     * @param number - player number
+     * @return player's username
      */
-    public PlayerInfo(final String username, final PlayerType type, final PlayerNumber number) {
-        this.username = username;
-        this.type = type;
-        this.number = number;
-    }
+    String getUsername();
 
     /**
      * @return player's username
      */
-    public String getUsername() {
-        return username;
-    }
+    PlayerType getType();
 
     /**
      * @return player's username
      */
-    public PlayerType getType() {
-        return type;
-    }
-
-    /**
-     * @return player's username
-     */
-    public PlayerNumber getNumber() {
-        return number;
-    }
+    PlayerNumber getNumber();
 }
