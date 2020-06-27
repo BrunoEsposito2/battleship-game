16bb8c2f490b5b8c45c86d5048b3c55c6ac98a56
Angelo Tinti <angelo.tinti@studio.unibo.it>
2020-06-27 15:21:44 +0200

    src/main/java/model/match/CellsAlreadyUsedException.java - Added javadoc.

--- src/main/java/model/match/CellsAlreadyUsedException.java
+++ src/main/java/model/match/CellsAlreadyUsedException.java
@@ -2,16 +2,17 @@ package model.match;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import model.util.Pair;
 
+/**
+ * 
+ * Exception representing situation where cells these it want to use are already used.
+ * 
+ */
 public class CellsAlreadyUsedException extends IOException {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 7451832879293050065L;
 
     private final List<Pair<Integer, Integer>> cellsUsed;
@@ -22,7 +23,11 @@ public class CellsAlreadyUsedException extends IOException {
         this.cellsUsed = cellsUsed;
     }
 
-
+    /**
+     * Get a list of cells already used.
+     * 
+     * @return list - list of cell already used
+     */
     public List<Pair<Integer, Integer>> getCellsUsed() {
         return new ArrayList<>(this.cellsUsed);
     }
