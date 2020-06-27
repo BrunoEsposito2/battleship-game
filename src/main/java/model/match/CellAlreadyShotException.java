785c7f588209e44b1423da3a368bdd7913ac23b2
Angelo Tinti <angelo.tinti@studio.unibo.it>
2020-06-27 15:21:30 +0200

    src/main/java/model/match/CellAlreadyShotException.java - Added javadoc.

--- src/main/java/model/match/CellAlreadyShotException.java
+++ src/main/java/model/match/CellAlreadyShotException.java
@@ -3,14 +3,19 @@ package model.match;
 import java.util.Arrays;
 import model.util.Pair;
 
-public class CellAlreadyShottedException extends CellsAlreadyUsedException {
+/**
+ * 
+ * Exception representing situation where cell is already shot.
+ * 
+ */
+public class CellAlreadyShotException extends CellsAlreadyUsedException {
 
     /**
      * 
      */
     private static final long serialVersionUID = 6085552272590623167L;
 
-    public CellAlreadyShottedException(final Pair<Integer, Integer> cellUsed) {
+    public CellAlreadyShotException(final Pair<Integer, Integer> cellUsed) {
         super(Arrays.asList(cellUsed));
     }
 
@@ -19,7 +24,7 @@ public class CellAlreadyShottedException extends CellsAlreadyUsedException {
      */
     @Override
     public String toString() {
-        return "Cell already shotted: [" + this.getCellsUsed().get(0).getX() + ", " 
+        return "Cell already shot: [" + this.getCellsUsed().get(0).getX() + ", " 
                 + this.getCellsUsed().get(0).getX() + "]" + super.toString(); 
     }
 
