32a728c408e21952236fbe5126148ab2fdfff4ae
Angelo Tinti <angelo.tinti@studio.unibo.it>
2020-06-30 00:08:38 +0200

    src/main/java/controller/game/MatchControllerImpl.java - Modified costants.

--- src/main/java/controller/game/MatchControllerImpl.java
+++ src/main/java/controller/game/MatchControllerImpl.java
@@ -33,8 +33,8 @@ public class MatchControllerImpl implements MatchController {
     /*
      * For now i put grid size here
      */
-    private static final int LINE = Battleships.getController().getMatchInfo().get().getFieldSize().getKey();
-    private static final int COLUMN = Battleships.getController().getMatchInfo().get().getFieldSize().getValue();
+    private static final int LINE = 10;
+    private static final int COLUMN = 10;
     private static final int SHIPS_NUMBER = Battleships.getController().getMatchInfo().get().getShipsNumber();
 
     private BattleView battleView;
